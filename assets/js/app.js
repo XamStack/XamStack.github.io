89bd1c4aafa1a0ad5f8a43d86c4abe87167ff284
Manas Khosla <43397881+ManasKhosla@users.noreply.github.com>
2022-11-23 02:23:28 +0530

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $('.collapsible').collapsible();
+});
+
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery);
